4085f4ceae8e235f3318546dd5e1a64320b3aaea
cbstodd <cbstodd@gmail.com>
2018-07-08 21:13:11 -0400

    Can now add new page

--- src/app/components/admin/admin-page/admin-page-new/admin-page-new.component.ts
+++ src/app/components/admin/admin-page/admin-page-new/admin-page-new.component.ts
@@ -1,15 +1,106 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Page } from '../../../../models/Page';
+import { AdminSettingsService } from '../../../../services/admin-settings.service';
+import { AuthService } from '../../../../services/auth.service';
+import { PageService } from '../../../../services/page.service';
 
 @Component({
-  selector: 'ddw-admin-page-new',
-  templateUrl: './admin-page-new.component.html',
-  styleUrls: ['./admin-page-new.component.css']
+    selector: 'ddw-admin-page-new',
+    templateUrl: './admin-page-new.component.html',
+    styleUrls: ['./admin-page-new.component.css']
 })
 export class AdminPageNewComponent implements OnInit {
+    page: Page;
+    newPageForm: FormGroup;
+    title: string;
+    author: string;
+    date: number = Date.now();
+    photoURL: string;
+    body: string;
+    category: string;
+    uid: string;
+    published: boolean;
+    disableAdminOnNew: boolean;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    constructor(
+      private pageService: PageService,
+      private router: Router,
+      private route: ActivatedRoute,
+      private flashMessage: FlashMessagesService,
+      private fb: FormBuilder,
+      private settingsService: AdminSettingsService,
+      private authService: AuthService
+    ) {
+        this.authService.getAuth().subscribe((auth) => {
+            if (auth) {
+                this.author = auth.email;
+            } else {
+
+            }
+        });
+    }
+
+    // For Form Validations
+    get f() {
+        return this.newPageForm.controls;
+    }
+
+    ngOnInit() {
+        // Settings
+        this.disableAdminOnNew = this.settingsService.getAdminSettings().disableAdmin;
+
+        // Form:
+        this.newPageForm = this.fb.group({
+            uid: [''],
+            title: ['',
+                    Validators.compose([
+                        Validators.required, Validators.minLength(10)
+                    ])
+            ],
+            body: ['',
+                   Validators.compose([
+                       Validators.required, Validators.minLength(100)
+                   ])
+            ],
+            author: ['' || this.author, Validators.required],
+            date: [Date.now()],
+            photoURL: ['' || 'https://higherlogicdownload.s3.amazonaws.com/GASTRO/44b1f1fd-aaed-44c8-954f-b0eaea6b0462/UploadedImages/interior-bg.jpg'],
+            category: [''],
+            published: [''],
+        });
+
+        this.uid = this.newPageForm.value.uid;
+        this.title = this.newPageForm.value.title;
+        this.body = this.newPageForm.value.body;
+        this.author = this.newPageForm.value.author;
+        this.date = this.newPageForm.value.date;
+        this.photoURL = this.newPageForm.value.photoURL;
+        this.category = this.newPageForm.value.category;
+        this.published = this.newPageForm.value.published;
+    }
+
+
+    onAddNewPage(formData) {
+        if (!this.newPageForm.valid) {
+            this.flashMessage.show('Something went wrong, Page was not created.',
+              {
+                  cssClass: 'alert-danger',
+                  timeout: 3500
+              });
+        } else {
+            this.pageService.addPage(formData);
+            // console.log(`${newPage.email}, ${newPage.password}, ${newPage.admin}`);
+            this.newPageForm.reset();
+            this.flashMessage.show(`${formData.title} was created!`,
+              {
+                  cssClass: 'alert-success',
+                  timeout: 3500
+              });
+        }
+    }
 
 }

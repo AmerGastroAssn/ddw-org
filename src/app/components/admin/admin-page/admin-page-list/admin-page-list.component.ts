8051fcc65c223fbc554a2360944f54adcb0980b1
cbstodd <cbstodd@gmail.com>
2018-07-16 23:47:31 -0400

    Can now toggle view of pages between list/table and cards

--- src/app/components/admin/admin-page/admin-page-list/admin-page-list.component.ts
+++ src/app/components/admin/admin-page/admin-page-list/admin-page-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Page } from '../../../../models/Page';
+import { User } from '../../../../models/User';
+import { AuthService } from '../../../../services/auth.service';
 import { PageService } from '../../../../services/page.service';
 
 @Component({
@@ -10,14 +13,38 @@ import { PageService } from '../../../../services/page.service';
 })
 export class AdminPageListComponent implements OnInit {
     page$: Observable<Page[]>;
+    pageList: Observable<Page[]>;
+    page: Page;
+    user: User;
+    id: any;
+    showListToggle: boolean;
 
     constructor(
-      private readonly pageService: PageService
+      private readonly pageService: PageService,
+      private authService: AuthService,
+      private route: ActivatedRoute,
     ) {
     }
 
     ngOnInit() {
         this.page$ = this.pageService.getPages();
+        this.user = this.authService.getProfile();
+        this.pageList = this.pageService.getAllRegisterPages();
+
+        // Get Page uid
+        // Get id from url
+        this.id = this.route.snapshot.params['id'];
+        // Get each user's details
+        this.pageService.getPage(this.id).subscribe((page) => {
+            if (page !== null) {
+                this.page = page;
+            }
+        });
+    }
+
+    toggleListCardView() {
+        this.showListToggle = !this.showListToggle;
+        console.log(this.showListToggle);
     }
 
 }

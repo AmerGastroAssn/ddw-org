4838d108b909df39f2f56d7039615413a34495b9
cbstodd <cbstodd@gmail.com>
2018-08-06 21:22:36 -0400

    Renamed branch

--- functions/index.js
+++ functions/index.js
@@ -12,22 +12,22 @@ const firestore = new Firestore();
 
 
 
-exports.onUserStatusChanged = functions.database
-  .ref('/status/{userId}') // Reference to the Firebase RealTime database key
-  .onUpdate(event => {
-      const usersRef = firestore.collection('/users'); // Create a reference to the Firestore Collection
-
-      return event.data.ref.once('value')
-        .then(statusSnapshot => snapShot.val()) // Get the latest value from the Firebase Realtime database
-        .then(status => {
-            // check if the value is 'offline'
-            if (status === 'offline') {
-                // Set the Firestore's document's online value to false
-                usersRef
-                  .doc(event.params.userId)
-                  .set({
-                      isOnline: false
-                  }, { merge: true });
-            }
-        })
-  });
+// exports.onUserStatusChanged = functions.database
+//   .ref('/status/{userId}') // Reference to the Firebase RealTime database key
+//   .onUpdate(event => {
+//       const usersRef = firestore.collection('/users'); // Create a reference to the Firestore Collection
+//
+//       return event.data.ref.once('value')
+//         .then(statusSnapshot => snapShot.val()) // Get the latest value from the Firebase Realtime database
+//         .then(status => {
+//             // check if the value is 'offline'
+//             if (status === 'offline') {
+//                 // Set the Firestore's document's online value to false
+//                 usersRef
+//                   .doc(event.params.userId)
+//                   .set({
+//                       isOnline: false
+//                   }, { merge: true });
+//             }
+//         })
+//   });

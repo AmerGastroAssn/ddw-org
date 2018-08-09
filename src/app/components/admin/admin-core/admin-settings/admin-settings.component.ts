77d615bcb9ce19e4f965d1a8391e8134b8a7c342
cbstodd <cbstodd@gmail.com>
2018-08-09 13:56:49 -0400

    Added skeleton (not functioning) for countdown datepicker form

--- src/app/components/admin/admin-core/admin-settings/admin-settings.component.ts
+++ src/app/components/admin/admin-core/admin-settings/admin-settings.component.ts
@@ -4,6 +4,7 @@ import { MatSnackBar } from '@angular/material';
 import { FlashMessagesService } from 'angular2-flash-messages';
 import { Settings } from '../../../../models/Settings';
 import { AdminSettingsService } from '../../../../services/admin-settings.service';
+import { CountdownService } from '../../../../services/countdown.service';
 
 @Component({
     selector: 'ddw-admin-settings',
@@ -12,18 +13,22 @@ import { AdminSettingsService } from '../../../../services/admin-settings.servic
 })
 export class AdminSettingsComponent implements OnInit {
     @ViewChild('settingsForm') settingsForm: NgForm;
+    @ViewChild('countdownForm') countdownForm: NgForm;
+
     settingsAllowed: boolean; // Allow settings to be edited/viewed.
 
     settings: Settings;
     allowSignup: boolean;
     allowSettings: boolean;
     disableAdmin: boolean;
+    countdown: string;
 
 
     constructor(
       public settingsService: AdminSettingsService,
       private flashMessage: FlashMessagesService,
       private sbAlert: MatSnackBar,
+      private countdownService: CountdownService
     ) {
     }
 
@@ -47,6 +52,13 @@ export class AdminSettingsComponent implements OnInit {
             verticalPosition: 'bottom',
             panelClass: ['snackbar-success']
         });
+    }
+
+    onCountdownSubmit(cdForm: NgForm) {
+        const value = cdForm.value;
+        // this.countdownService.saveCountdown(value);
+        console.log(value);
+
     }
 
 }

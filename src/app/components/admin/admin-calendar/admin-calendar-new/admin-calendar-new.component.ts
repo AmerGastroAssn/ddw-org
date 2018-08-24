f94932709224fa4298d557c81d4744b734eee8f6
cbstodd <cbstodd@gmail.com>
2018-08-23 23:19:32 -0400

    Timestamps needed to be type string

--- src/app/components/admin/admin-calendar/admin-calendar-new/admin-calendar-new.component.ts
+++ src/app/components/admin/admin-calendar/admin-calendar-new/admin-calendar-new.component.ts
@@ -41,10 +41,10 @@ export class AdminCalendarNewComponent implements OnInit {
     body2: string;
     body3: string;
     body4: string;
-    date1: number;
-    date2: number;
-    date3: number;
-    date4: number;
+    date1: string;
+    date2: string;
+    date3: string;
+    date4: string;
     title: string;
     uid: string;
     color = 'primary';

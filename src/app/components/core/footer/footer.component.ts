f8a86d9aebb9445cc7ca67fdaae9c1d223cf2e08
cbstodd <cbstodd@gmail.com>
2019-07-26 12:12:47 -0400

    Fixed issue where ads were being cut off in desktop view

--- src/app/components/core/footer/footer.component.ts
+++ src/app/components/core/footer/footer.component.ts
@@ -15,8 +15,11 @@ export class FooterComponent implements OnInit {
     exhibitorInfoPages: Page[];
     newsAndMediaPages: Page[];
     presentersPages: Page[];
+    logo: string;
 
     constructor(private pageService: PageService) {
+        // tslint:disable-next-line:max-line-length
+        this.logo = 'https://firebasestorage.googleapis.com/v0/b/ddw-org.appspot.com/o/images%2F2019%2F1562782847583_ddw2020_logo_yellow_rgb_r.png?alt=media&token=6d7ae798-2170-4ad6-acef-472b8be1fea3';
     }
 
     ngOnInit() {
@@ -25,34 +28,34 @@ export class FooterComponent implements OnInit {
          by their 'sortOrder' value.
          ------------------------------------------------*/
         this.pageService.getRegisterPages()
-            .subscribe((pageArr) => {
-                this.registerPages = _.orderBy(pageArr, ['sortOrder'], ['asc']);
-            });
+          .subscribe((pageArr) => {
+              this.registerPages = _.orderBy(pageArr, ['sortOrder'], ['asc']);
+          });
 
         this.pageService.getAttendeePlanningPages()
-            .subscribe((pageArr) => {
-                this.attendeePlanningPages = _.orderBy(pageArr, ['sortOrder'], ['asc']);
-            });
+          .subscribe((pageArr) => {
+              this.attendeePlanningPages = _.orderBy(pageArr, ['sortOrder'], ['asc']);
+          });
 
         this.pageService.getEducationPages()
-            .subscribe((pageArr) => {
-                this.educationPages = _.orderBy(pageArr, ['sortOrder'], ['asc']);
-            });
+          .subscribe((pageArr) => {
+              this.educationPages = _.orderBy(pageArr, ['sortOrder'], ['asc']);
+          });
 
         this.pageService.getExhibitorInfoPages()
-            .subscribe((pageArr) => {
-                this.exhibitorInfoPages = _.orderBy(pageArr, ['sortOrder'], ['asc']);
-            });
+          .subscribe((pageArr) => {
+              this.exhibitorInfoPages = _.orderBy(pageArr, ['sortOrder'], ['asc']);
+          });
 
         this.pageService.getNewsAndMediaPages()
-            .subscribe((pageArr) => {
-                this.newsAndMediaPages = _.orderBy(pageArr, ['sortOrder'], ['asc']);
-            });
+          .subscribe((pageArr) => {
+              this.newsAndMediaPages = _.orderBy(pageArr, ['sortOrder'], ['asc']);
+          });
 
         this.pageService.getPresenterPages()
-            .subscribe((pageArr) => {
-                this.presentersPages = _.orderBy(pageArr, ['sortOrder'], ['asc']);
-            });
+          .subscribe((pageArr) => {
+              this.presentersPages = _.orderBy(pageArr, ['sortOrder'], ['asc']);
+          });
     }
 
 }
